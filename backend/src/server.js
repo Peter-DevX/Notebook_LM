de97e376a4ca6092d4190f45e7fdfff891ad304f
Peter-DevX <peterasiedugyan0@gmail.com>
2026-02-15 04:01:40 +0000

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,23 +4,38 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimit from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 const app = express();
-
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+console.log(__dirname);
 
 // Middleware used to get user's input
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if(process.env.NODE_ENV !== "production"){
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+
+}
+
+
 app.use(express.json());
 app.use(rateLimit);
 
 // Entry API
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // DB connection and running server
 connectDB().then(() => {
